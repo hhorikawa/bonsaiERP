cb98fdda65f609b23d7745fb800e63cdf25b4aaa
Hisashi Horikawa <hisashi.horikawa@gmail.com>
2025-10-05 21:54:56 +0900

    Delete app/controllers/helper.rb

--- app/controllers/helper.rb
+++
@@ -1,7 +0,0 @@
-class Helper
-  include Singleton
-  include ActionView::Helpers::TagHelper
-  include ActionView::Helpers::UrlHelper
-  include ActionView::Helpers::NumberHelper
-  #include ActionView::Partials
-end
